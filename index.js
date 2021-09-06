8e9b2a1cad8c63b7feeaf23d3b49a059742835f2
Vahagn Saribekyan <Vahag>
2021-09-06 20:36:48 +0400

    updated handler

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 'use strict';
 
-const { dataHandler } = require('./src/datahandler');
-const { environments } = require('./src/constants');
-
 const path = require('path');
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === environments.dev) {
   require('dotenv').config({ path: path.resolve(process.cwd(), '.env-dev') });
 }
 
+const { dataHandler } = require('./src/datahandler');
+const { environments } = require('./src/constants');
+
 exports.handler = async (event) => {
   try {
     await dataHandler();
     console.log('Events successfully processed');
   } catch(err) {
-    console.error('There was an error while processing events', err.message);
+    console.error('There was an error while processing events: ', err.message);
   }
 
   return {
     statusCode: 200
   };
 };
+
+exports.handler();
--- src/datahandler/handler.js
+++ src/datahandler/handler.js
@@ -4,10 +4,22 @@ const datastore = require('../datastore');
 const config = require('../config');
 const { markerKey } = require('../constants');
 
-/* delete later TODO
-const path = require('path');
-require('dotenv').config({ path: path.resolve(process.cwd(), '.env-dev') });
-*/
+const { validate: validateUuid } = require('uuid');
+
+const validateTimestamp = (timestamp) => {
+  return (new Date(timestamp)).getTime() > 0;
+}
+
+const parseEvents = (events) => {
+  return events.map((event) => {
+    try {
+      const e = JSON.parse(event);
+      return e;
+    } catch (e) {
+      console.debug('Invalid event: ', event);
+    }
+  }).filter(e => e);
+}
 
 const getMarker = async () => {
   let marker = null;
@@ -20,19 +32,48 @@ const getMarker = async () => {
   return marker;
 }
 
+// TODO: fix issue
+const normalizeEvents = (events) => {
+  let validEvents = [];
+  for (let event of events) {
+    if (validateUuid(event['user-uuid']) && (new Date(event.timestamp)).getTime() > 0) {
+      validEvents.push(event);
+    }
+  }
+
+  return validEvents;
+}
+
+// TODO finalize issue
+const aggregateEvents = (events) => {
+  return events;
+}
+
 module.exports = async () => {
   // Read the marker
   const marker = await getMarker();
 
   // Read the events
   const eventsPromise = await datastore.read({ bucketName: config.awsS3BucketName, token: marker })
-  const events = await eventsPromise.data;
+  let events = await eventsPromise.data;
+  // resolve bodies
+  events = events.map(e => e.Body.toString());
+  // parse bodies
+  events = parseEvents(events);
+  // flatten the nested events
+  events = events.flat(Infinity);
 
   // Save next marker
   const nextMarker = eventsPromise.nextContinuationToken;
   if (nextMarker) {
     await datastore.write({ bucketName: config.awsS3BucketName, key: markerKey, data: nextMarker });
   }
+
+  // Normalize events
+  events = normalizeEvents(events);
+
+  // Aggregate events
+  events = aggregateEvents(events);
 
   // TODO: save into dynamo db
   console.log(events);
