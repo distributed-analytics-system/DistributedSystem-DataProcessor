06f2dfb181cd19b02257907054fdcc74a4cc6edb
Vahagn Saribekyan <Vahag>
2021-09-07 13:10:54 +0400

    Added aggregatoion logic

--- src/datahandler/handler.js
+++ src/datahandler/handler.js
@@ -7,6 +7,7 @@ const { markerKey, databaseTableName } = require('../constants');
 
 const { validate: validateUuid } = require('uuid');
 const moment = require('moment');
+const _ = require('lodash');
 
 const validateTimestamp = (timestamp) => {
   const date = moment.unix(timestamp / 1000);
@@ -50,12 +51,54 @@ const normalizeEvents = (events) => {
   return validEvents;
 }
 
-// TODO add aggregation logic
 const aggregateEvents = (events) => {
+  // group elements by user-uuid
+  events = _.mapValues(_.groupBy(events, 'user-uuid'), clist => clist.map(event => _.omit(event, 'user-uuid')));
+
+  const sortOperand = (a, b) => {
+    if (parseInt(a.timestamp) < parseInt(b.timestamp)) {
+      return -1;
+    }
+    if (parseInt(a.timestamp) > parseInt(b.timestamp)){
+      return 1;
+    }
+
+    return 0;
+  }
+
+  // calculate clicks count and time spent
+  for (let key in events) {
+    // sort events by timestamp
+    events[key] = events[key].sort(sortOperand);
+
+    // calculate clicks count
+    let groupedClicks = _.mapValues(_.groupBy(events[key], 'screen'), clist => clist.map(click => _.omit(click, 'screen')));
+    for (let c in groupedClicks) {
+      const clickCount = groupedClicks[c].length;
+      groupedClicks[c] = { clicks: clickCount, timespent: 0 };
+    }
+
+    // calculate time spent
+    if (Array.isArray(events[key]) && events[key].length !== 0) {
+      let lastUsedScreen = events[key][0].screen;
+      let firstT = events[key][0].timestamp;
+      for (let i = 1; i < events[key].length; ++i) {
+        if(lastUsedScreen !== events[key][i].screen) {
+          groupedClicks[lastUsedScreen].timespent += events[key][i].timestamp - firstT;
+
+          lastUsedScreen = events[key][i].screen;
+          firstT = events[key][i].timestamp;
+        }
+      }
+    }
+
+    events[key] = groupedClicks;
+  }
+
   return events;
 }
 
-module.exports = async () => {
+const handler = async () => {
   // Read the marker
   const marker = await getMarker();
 
@@ -82,6 +125,8 @@ module.exports = async () => {
   events = aggregateEvents(events);
 
   // TODO: save into dynamo db
-  await database.save({ tableName: databaseTableName, data: events });
+  // await database.save({ tableName: databaseTableName, data: events });
   console.log(events);
 }
+
+module.exports = handler;
